3c45fa0d9ef6ff04454b550241aacb6ec6450722
coolq1000 <coolq1000@gmail.com>
2017-08-28 10:05:16 +1000

    Added comments, better names, demo, more features.
    Added comments.
    Added more pythonic_names.
    Added a demo at the bottom.
    Added more methods, i.e. get_profile and get_search.

--- DevrantAPI/devrant.py
+++ DevrantAPI/devrant.py
@@ -1,8 +1,7 @@
 #! /usr/bin/python3
 
 """
-	Devrant.io API.
-	Has a way to go, only allows getting rants and userID's.
+	Devrant.io API. Written by Rohan Burke (coolq).
 """
 
 import requests, json
@@ -12,14 +11,48 @@ class Devrant:
 	API = 'https://www.devrant.io/api/'
 
 	"""
-		getRant(): Seems to only work to ~788.
+		get_profile(id):
+			Return JSON object with all information about that user.
 	"""
-	def getRant(self, sort, index):
-		rants = self.getRants(sort, 1, index)['rants']
+	def get_profile(self, id_):
+		url = self.API + 'users/' + str(id_)
+		params = {
+			'app': 3,
+		}
+
+		r = requests.get(url, params)
+		obj = json.loads(r.text)
+		return obj
+
+	"""
+		get_search(term):
+			Return JSON object containing all results of that search. Index ['rants'] for rants.
+	"""
+	def get_search(self, term):
+		url = self.API + 'devrant/search'
+		params = {
+			'app': 3,
+			'term': term
+		}
+		
+		r = requests.get(url, params)
+		obj = json.loads(r.text)
+		return obj
+
+	"""
+		get_rant(sort, index):
+			Return JSON object of that rant.
+	"""
+	def get_rant(self, sort, index):
+		rants = self.get_rants(sort, 1, index)['rants']
 		if rants:
 			return rants[0]
 
-	def getRants(self, sort, limit, skip):
+	"""
+		get_rants(sort, limit, skip):
+			Return JSON object with range skip-limit. Max limit is 50, increase the skip to get rants further down.
+	"""
+	def get_rants(self, sort, limit, skip):
 		url = self.API + 'devrant/rants'
 		params = {
 			'app': 3,
@@ -32,7 +65,11 @@ class Devrant:
 		obj = json.loads(r.text)
 		return obj
 
-	def getUserID(self, name):
+	"""
+		get_user_id(name):
+			Return JSON with containing the id for that user, E.g. if `coolq` is inputted, it shall return `{'success': True, 'user_id': 703149}`.
+	"""
+	def get_user_id(self, name):
 		url = self.API + 'get-user-id'
 		params = {
 			'app': 3,
@@ -40,4 +77,19 @@ class Devrant:
 		}
 
 		r = requests.get(url, params)
-		return r.text
+		obj = json.loads(r.text)
+		return obj
+
+if __name__ == '__main__':
+	# Simple demo, runs through rants sorted by most recent.
+	dr = Devrant()
+	i = 0
+	while True:
+		result = dr.get_rant('recent', i)
+		print('\n'*50)
+		name = result['user_username']
+		tags = ', '.join(result['tags'])
+		print('-' + name + '-'*(50 - (len(name) + 1)))
+		print(result['text'])
+		print('-' + tags + '-'*(50 - (len(tags) + 1)))
+		i += 1
